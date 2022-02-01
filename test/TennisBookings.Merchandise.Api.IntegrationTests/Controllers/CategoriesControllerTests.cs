e88946221ea10991f53ac2e8e0b2c55e9fc6cf6a
muratilgun <muratilgun34@gmail.com>
2022-02-02 01:35:40 +0300

    Create CategoriesControllerTests.cs

---
+++ test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/CategoriesControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace TennisBookings.Merchandise.Api.IntegrationTests.Controllers
+{
+    public class CategoriesControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+        public CategoriesControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsSuccessStatus()
+        {
+            var response = await _client.GetAsync("api/Categories");
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsExpectedMediaType()
+        {
+            var response = await _client.GetAsync("api/Categories");
+            Assert.Equal("application/json",response.Content.Headers.ContentType.MediaType);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsContent()
+        {
+            var response = await _client.GetAsync("api/Categories");
+            Assert.NotNull(response.Content);
+            Assert.True(response.Content.Headers.ContentLength > 0);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsExpectedJson()
+        {
+            var response = await _client.GetStringAsync("api/Categories");
+            Assert.Equal("{\"allowedCategories\":[\"Accessories\",\"Bags\",\"Balls\",\"Clothing\",\"Rackets\"]}",response);
+        }
+
+
+    }
+}
