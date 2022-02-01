75a2f7a6e24ce0cdee073b4425d5a438b7991339
muratilgun <muratilgun34@gmail.com>
2022-02-01 23:31:36 +0300

    Created a unit test class using the WebApplicationFactory class fixture
    Created a new project using the xUnit template
    Updated the project to use the web SDK "Microsoft.NET.Sdk.Web"
    Referenced Microsoft.AspNetCore.Mvc.Testing NuGet package
    Disabled shadow copying using xunit.runner.json

---
+++ test/TennisBookings.Merchandise.Api.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace TennisBookings.Merchandise.Api.IntegrationTests
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public HealthCheckTests(WebApplicationFactory<Startup> factory)
+        {
+            _httpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task HealthCheck_RetunsOk()
+        {
+            var response = await _httpClient.GetAsync("/healthcheck");
+            Assert.Equal(HttpStatusCode.OK,response.StatusCode);
+        }
+    }
+}
