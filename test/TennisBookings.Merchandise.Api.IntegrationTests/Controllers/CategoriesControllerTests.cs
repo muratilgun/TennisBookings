b95afe11919693bc64309dd777897dbff50dd72d
muratilgun <muratilgun34@gmail.com>
2022-02-02 13:51:31 +0300

    Simplifying Response Assertions

--- test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/CategoriesControllerTests.cs
+++ test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/CategoriesControllerTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
@@ -23,40 +24,50 @@ namespace TennisBookings.Merchandise.Api.IntegrationTests.Controllers
             _client = factory.CreateClient();
         }
 
-        [Fact]
-        public async Task GetAll_ReturnsSuccessStatus()
-        {
-            var response = await _client.GetAsync("");
-            response.EnsureSuccessStatusCode();
-        }
+        #region OLD TESTS
+        //[Fact]
+        //public async Task GetAll_ReturnsSuccessStatus()
+        //{
+        //    var response = await _client.GetAsync("");
+        //    response.EnsureSuccessStatusCode();
+        //}
+
+        //[Fact]
+        //public async Task GetAll_ReturnsExpectedMediaType()
+        //{
+        //    var response = await _client.GetAsync("");
+        //    Assert.Equal("application/json",response.Content.Headers.ContentType.MediaType);
+        //}
+
+        //[Fact]
+        //public async Task GetAll_ReturnsContent()
+        //{
+        //    var response = await _client.GetAsync("");
+        //    Assert.NotNull(response.Content);
+        //    Assert.True(response.Content.Headers.ContentLength > 0);
+        //}
+
+        //[Fact]
+        //public async Task GetAll_ReturnsExpectedJson()
+        //{
+        //    //var response = await _client.GetStringAsync("api/Categories");
+        //    //Assert.Equal("{\"allowedCategories\":[\"Accessories\",\"Bags\",\"Balls\",\"Clothing\",\"Rackets\"]}",response);
+        //    var expected = new List<string> { "Bags", "Balls", "Accessories", "Clothing", "Rackets" };
+        //    var responseStream = await _client.GetStreamAsync("");
+        //    var model = await JsonSerializer.DeserializeAsync<ExpectedCategoriesModel>(responseStream,JsonSerializerHelper.DefaultDeserialisationOptions);
+        //    Assert.NotNull(model?.AllowedCategories);
+        //    Assert.Equal(expected.OrderBy(s=> s),model.AllowedCategories.OrderBy(s=> s));
+        //} 
+        #endregion
 
         [Fact]
-        public async Task GetAll_ReturnsExpectedMediaType()
+        public async Task GetAll_ReturnsExpectedResponse()
         {
-            var response = await _client.GetAsync("");
-            Assert.Equal("application/json",response.Content.Headers.ContentType.MediaType);
-        }
-
-        [Fact]
-        public async Task GetAll_ReturnsContent()
-        {
-            var response = await _client.GetAsync("");
-            Assert.NotNull(response.Content);
-            Assert.True(response.Content.Headers.ContentLength > 0);
-        }
-
-        [Fact]
-        public async Task GetAll_ReturnsExpectedJson()
-        {
-            //var response = await _client.GetStringAsync("api/Categories");
-            //Assert.Equal("{\"allowedCategories\":[\"Accessories\",\"Bags\",\"Balls\",\"Clothing\",\"Rackets\"]}",response);
             var expected = new List<string> { "Bags", "Balls", "Accessories", "Clothing", "Rackets" };
-            var responseStream = await _client.GetStreamAsync("");
-            var model = await JsonSerializer.DeserializeAsync<ExpectedCategoriesModel>(responseStream,JsonSerializerHelper.DefaultDeserialisationOptions);
+            var model = await _client.GetFromJsonAsync<ExpectedCategoriesModel>("");
             Assert.NotNull(model?.AllowedCategories);
-            Assert.Equal(expected.OrderBy(s=> s),model.AllowedCategories.OrderBy(s=> s));
+            Assert.Equal(expected.OrderBy(s => s), model.AllowedCategories.OrderBy(s => s));
+
         }
-
-
     }
 }
