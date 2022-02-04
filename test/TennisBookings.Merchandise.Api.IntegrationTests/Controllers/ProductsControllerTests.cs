9f4ab89ad201055efb7a9f8fd5381d9d09db63a3
muratilgun <muratilgun34@gmail.com>
2022-02-04 23:31:17 +0300

    Test Model Binding and Input Validation for POST Requests

--- test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/ProductsControllerTests.cs
+++ test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/ProductsControllerTests.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using TennisBookings.Merchandise.Api.Data;
 using TennisBookings.Merchandise.Api.Data.Dto;
 using TennisBookings.Merchandise.Api.External.Database;
 using TennisBookings.Merchandise.Api.IntegrationTests.Fakes;
 using TennisBookings.Merchandise.Api.IntegrationTests.Models;
+using TennisBookings.Merchandise.Api.IntegrationTests.TestHelpers;
 using Xunit;
 
 namespace TennisBookings.Merchandise.Api.IntegrationTests.Controllers
@@ -48,5 +52,43 @@ namespace TennisBookings.Merchandise.Api.IntegrationTests.Controllers
             Assert.NotNull(product);
             Assert.Equal(firstProduct.Name, product.Name);
         }
+
+        [Fact]
+        public async Task Post_WithoutName_ReturnsBadRequest()
+        {
+            var productInputModel = GetValidProductInputModel().CloneWith(m => m.Name = null);
+            var response = await _client.PostAsJsonAsync("", productInputModel,JsonSerializerHelper.DefaultSerialisationOptions);
+            Assert.Equal(HttpStatusCode.BadRequest,response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_WithInvalidName_ReturnsExpectedProblemDetails()
+        {
+            var productInputModel = GetValidProductInputModel().CloneWith(m => m.Name = null);
+            var response = await _client.PostAsJsonAsync("", productInputModel, JsonSerializerHelper.DefaultSerialisationOptions);
+            var problemDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+            Assert.Collection(problemDetails.Errors, kvp =>
+            {
+                Assert.Equal("Name",kvp.Key);
+                var error = Assert.Single(kvp.Value);
+                Assert.Equal("The Name field is required.",error);
+            });
+
+        }
+
+
+        private static TestProductInputModel GetValidProductInputModel(Guid? id = null)
+        {
+            return new TestProductInputModel
+            {
+                Id = id is object ? id.Value.ToString() : Guid.NewGuid().ToString(),
+                Name = "Some Product",
+                Description = "This is a description",
+                Category = new CategoryProvider().AllowedCategories().First(),
+                InternalReference = "ABC123",
+                Price = 4.00m
+            };
+        }
+
     }
 }
