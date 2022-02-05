43fd2b61293b963c83653037d5268d82a94d5161
muratilgun <muratilgun34@gmail.com>
2022-02-05 23:52:34 +0300

    Testing Response for Post Requests

--- test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/ProductsControllerTests.cs
+++ test/TennisBookings.Merchandise.Api.IntegrationTests/Controllers/ProductsControllerTests.cs
@@ -97,6 +97,39 @@ namespace TennisBookings.Merchandise.Api.IntegrationTests.Controllers
             Assert.Collection(problemDetails.Errors,validator);
 
         }
+
+        [Fact]
+        public async Task Post_WithExistingProductId_ReturnsConflict_WithExpectedLocation()
+        {
+            var id = _factory.FakeCloudDatabase.Products.First().Id;
+            var content = GetValidProductJsonContent(id);
+            var response = await _client.PostAsync("",content);
+            Assert.Equal(HttpStatusCode.Conflict,response.StatusCode);
+            Assert.Equal($"http://localhost/api/products/{id}",response.Headers.Location.ToString().ToLower());
+        }
+
+        [Fact]
+        public async Task Post_WithValidProduct_ReturnsCreatedResult()
+        {
+            var id = Guid.NewGuid();
+            var content = GetValidProductJsonContent(id);
+            var response = await _client.PostAsync("", content);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal($"http://localhost/api/products/{id}", response.Headers.Location.ToString().ToLower());
+        }
+
+
+        [Fact]
+        public async Task Post_AfterPostingValidProduct_ItCanBeRetrieved()
+        {
+            var id = Guid.NewGuid(); 
+            var content = GetValidProductJsonContent(id);
+            var response = await _client.PostAsync("", content);
+            response.EnsureSuccessStatusCode();
+            var getResponse = await _client.GetAsync(response.Headers.Location.ToString());
+            getResponse.EnsureSuccessStatusCode();
+        }
+
         private static JsonContent GetValidProductJsonContent(Guid? id = null)
         {
             return JsonContent.Create(GetValidProductInputModel(id));
