e7ccb4dc32fa05d4109b569a44d76f5c38fef455
muratilgun <muratilgun34@gmail.com>
2022-02-07 21:56:57 +0300

    Configuring Entity Framework for In-memory Testing

--- test/TennisBookings.Web.IntegrationTests/CustomWebApplicationFactory.cs
+++ test/TennisBookings.Web.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TennisBookings.Web.Data;
 using TennisBookings.Web.External;
 using TennisBookings.Web.IntegrationTests.Fakes;
 using TennisBookings.Web.IntegrationTests.Helpers;
@@ -11,6 +17,7 @@ namespace TennisBookings.Web.IntegrationTests
 {
     public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
+        private readonly InMemoryDatabaseRoot _dbRoot = new InMemoryDatabaseRoot();
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment("Production");
@@ -19,6 +26,39 @@ namespace TennisBookings.Web.IntegrationTests
             {
                 services.AddSingleton<IWeatherApiClient, FakeWithDataWeatherApiClient>();
                 services.AddSingleton<IDateTime, FixedDateTime>();
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<TennisBookingDbContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<TennisBookingDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting",_dbRoot);
+                });
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<TennisBookingDbContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        DatabaseHelper.InitialiseDbForTests(db);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred seeding the " +
+                                            "database with test data. Error: {Message}", ex.Message);
+                    }
+                }
             });
         }
     }
